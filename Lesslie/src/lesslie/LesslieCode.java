13169213445471922e7c3f86244467be93e602d3
Ilyas Göcmenoglu <75431280+Ilyas-goc@users.noreply.github.com>
2020-12-08 11:31:38 +0100

    Add files via upload

--- Lesslie/src/lesslie/LesslieCode.java
+++ Lesslie/src/lesslie/LesslieCode.java
@@ -64,8 +64,8 @@ public class LesslieCode {
             return fib(n);
         }
         public Integer fib(int n) {
-            if (n <= 2)
-                return 1;
+            if (n < 2)
+                return n;
             return fib(n - 1) + fib(n - 2);
         }
     }
